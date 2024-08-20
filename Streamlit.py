76b7668455c33031929adcb9dbf47a242139bd58
Grant Doherty <grantdoherty79@gmail.com>
2024-08-20 16:06:18 -0400

    Streamlit update

--- Streamlit.py
+++ Streamlit.py
@@ -106,7 +106,7 @@ def payoff(S, r, T, K):
 S0 = 100.0          
 v0 = 0.06           
 T = 1.0             
-r = .02             
+r = .15             
 rho = -0.7          
 
 k = 3               
@@ -132,9 +132,9 @@ with st.sidebar:
     S0 = st.number_input("Initial Asset Price $S_0$ (dollars)", value=S0)
     v0 = st.number_input("Initial Volatility $v_0$ (years$^{-1}$)", value=v0)
     T = st.number_input("Time to Maturity $T$ (years)", value=T)
-    r = st.number_input("Risk-Free Rate", value=r)
-    rho = st.number_input("Wiener Correlation Factor", value=rho)
-    K = st.number_input("Strike Price", value=S0*1.3)
+    r = st.number_input("Risk-Free Rate $r$", value=r)
+    rho = st.number_input("Wiener Correlation Factor $\\rho$", value=rho)
+    K = st.number_input("Strike Price $K$", value=S0*1.3)
 
     st.markdown("<p style='font-size: 20px;'><strong>Fixed Parameters</strong></p>",
     unsafe_allow_html=True)
@@ -151,9 +151,9 @@ with st.sidebar:
 
 st.markdown("")
 st.header("Background Information")
-st.markdown("""The Heston Model is defined by set of stochastic differential equations (SDEs) describes how the price and volatility of an asset, security or stock could mature on the stock market
-            over some time interval $T$. The instantaneous change in the price, and volatility of the asset at time $t$ ($dS_t$ and $dv_t$ respectively) are governed by 
-            [Ornstein-Uhlenbeck processes](https://en.wikipedia.org/wiki/Ornstein%E2%80%93Uhlenbeck_process) (Equations 1 and 2).""", unsafe_allow_html=True)
+st.markdown("""The Heston Model is defined by a set of stochastic differential equations (SDEs) that describe how the price and volatility of an asset could mature on the stock market
+            over some time interval $T$. The instantaneous change in the price, and volatility of the asset at time $t$ ($dS_t$ and $dv_t$, respectively) are governed by 
+            [Ornstein-Uhlenbeck processes](https://en.wikipedia.org/wiki/Ornstein%E2%80%93Uhlenbeck_process) (Equations 1 and 2)""", unsafe_allow_html=True)
 
 ## dS_t and dv_t 
 st.markdown("""
@@ -168,8 +168,8 @@ dv_t = \kappa (\\theta - v_t)dt + \sigma \sqrt{v_t}dW_{v,t} \quad (2)
 $$
 """, unsafe_allow_html=True)
 
-st.markdown("""Where the Wiener processes $dW_{S,t}$ and $dW_{v,t}$ (for asset price and volatility respectively) are Monte Carlo processes to govern the "random walk" motion of stocks.These two 
-            Wiener processes are correlated by a factor $\\rho$, as detailed by Equation 3""", unsafe_allow_html=True)
+st.markdown("""where the Wiener processes $dW_{S,t}$ and $dW_{v,t}$ (for asset price and volatility, respectively) are Monte Carlo processes to govern the "random" motion of stocks. These two 
+            Wiener processes are correlated by $\\rho$, as detailed by Equation 3""", unsafe_allow_html=True)
 
 ## dWv*dWS = pdt
 st.markdown("""
@@ -179,11 +179,12 @@ $$
 """, unsafe_allow_html=True)
 
 st.markdown("""$S_t$ and $v_t$ are price and volatility of the asset at time $t$, and $dt$ is the difference in time between successive solutions. The risk-free rate $r$ (drift constant), 
-            mean reversion rate of the volatility, long-term mean of the volatility, $\\theta$, and the volatility of the volatility $\\sigma$ are simulation parameters that are set by the user (see right banner).
+            mean reversion rate of the volatility \\kappa, long-term mean of the volatility $\\theta$, and the volatility of the volatility $\\sigma$ are fixed simulation parameters
+            (see right banner).
             """, unsafe_allow_html=True)
 
 st.markdown("""This simulation relies on calculating the instantaneous asset volatility to update the price of the volatility in successive time steps using the Euler-discretized form of the
-             Heston model.""", unsafe_allow_html=True)
+             Heston model""", unsafe_allow_html=True)
 
 ## v_{t+1} and S_{t+1}
 st.markdown("""   
@@ -243,10 +244,10 @@ col1, col2 = st.columns([1,1], gap="small")
 # On the left side...
 with col1:
     # Info on the wiener correlation factor rho. 
-    st.markdown(f"""<br><br>The effects of the correlation factor $\\rho$ can be observed by overlaying the price and volatility curves of a single simulation
-                A negative correaltion $\\rho < 0$ causes the price and volatility to follow opposing trends, that is, when the volatility of the stock is high, 
-                the asset price tends to fall, and vice versa. Conversely, when $\\rho > 0$, the $S_t$ and $v_t$ curves tend to follow each other. Negative correlations
-                are typically observed on the stock market due to the leverage effect and risk aversion, however this not always the case.""", unsafe_allow_html=True)
+    st.markdown(f"""<br><br>The effects of the correlation factor $\\rho$ can be observed by overlaying the price and volatility curves of a single simulation.
+                A negative correaltion ($\\rho < 0$) causes the price and volatility to follow opposing trends, that is, when the volatility of the stock is high, 
+                the asset price tends to fall, and vice versa. Conversely, a positive correlation ($\\rho > 0$) causes the $S_t$ and $v_t$ curves to follow each other. 
+                Negative correlations are typically observed on the stock market due to the leverage effect and risk aversion.""", unsafe_allow_html=True)
     
     st.markdown(f"""**Try for yourself:** Adjust the $\\rho$ parameter on the left banner between positive and negative values to see how it affects the correlation between $S_t$ and $v_t$. \
                 Note that larger (more positive or more negative) values of $\\rho$ will intensify the correlation.""", unsafe_allow_html=True)
@@ -295,20 +296,21 @@ with col2:
 st.header("Options")
 # Explaining what options are
 st.markdown("""
-Options are financial contracts made between trading entities that give buyers the right, but not the obligation to buy or sell an underlying asset at a specific price, or at a later\
-             agreed-upon date. Options can be sold as a call option, or a put option:
+Options are contracts that give the owner (the holder of the option) the right, but not the obligation, to buy or sell a specific quantity of an
+             underlying asset at a specific strike price, on or before a specified date, depending on the type of option.
+             Options can be sold as a call option, or a put option:
 
 - **Call Option:** gives the holder the right to buy an underlying asset at a specific price (strike price).
 - **Put Option:** gives the holder the right to sell an underlying asset at a specific price.
 
-There are two common types of option contracts: American, and European options. American options can be exercised at any point in time up until the date of expiration, while \
+There are two common types of option contracts: American, and European options. American options can be exercised at any time up to the date of expiration, while \
             European options can only be exercised at the expiration date. Check out this [great YouTube video](https://youtu.be/VJgHkAqohbU?t=174) by The Plain Bagel for more information on options.
 """, unsafe_allow_html=True)
 
 st.subheader("Pricing Options")
 # Why we need to price options.
-st.markdown("""The price of a call/put option is not strictly defined like the price of a stock is, and institutions rely on models to price options.
-            In this simulation, the average call/put payoff can be calculated by averaging the individual call/put payoffs.
+st.markdown("""Unlike stocks, the price of an option is not strictly defined on a market, and institutions rely on models to price options.
+            This simulaton can be used as a tool to price European options. In this simulation, the average call/put payoff can be calculated by averaging the individual call/put payoffs.
 """, unsafe_allow_html=True)
 
 # EQ Av Call/Put Payoff
@@ -330,38 +332,41 @@ st.markdown("""where $S_T^i$ is the $i^{\\text{th}}$ asset price at maturiton, $
 # EQ Theoretical Call/Put Price
 st.markdown("""
     $$
-    \\text{Call Price} = e^{-rT} \\times \\text{Average Call Payoff} \qquad (6)
+    \\text{Call Price} = e^{-rT} \\times \\text{Average Call Payoff} \qquad (8)
     $$
 """, unsafe_allow_html=True)
 st.markdown("""
     $$
-    \\text{Put Price} = e^{-rT} \\times \\text{Average Put Payoff} \qquad (7)
+    \\text{Put Price} = e^{-rT} \\times \\text{Average Put Payoff} \qquad (9)
     $$
 """, unsafe_allow_html=True)
 
+N_steps_const = 250
+N_sims_const = 500
+
 st.markdown(rf"""
-    The table below represents the theoretical value of the call and put given the state of the parameters set to the left. Since the prices are dependent on averages,
-            the law of large numbers applies, and as a result, a higher number of simulations is desirable. The following pricing is calculated using 
-            $N_{{sims}}={N_sims}$ and $N_{{steps}}={N_steps}$.
+    The table below represents the simulated value of the call and put given the state of the parameters set to the left. Since the Wiener processes draw from standard normal distributions and
+             prices are dependent on averages, the law of large numbers applies and it is desirable to have a large number of simulations to get accurate pricing. The following pricing is calculated using 
+            $N_{{sims}}={N_sims_const}$ and $N_{{steps}}={N_steps_const}$.
 """, unsafe_allow_html=True)
 
 st.markdown(rf"""**NOTE:** This page compiles linearly and the information below must be computed every time variables are updated. Expect computation time delays (no longer than 30s).
 """, unsafe_allow_html=True)
 
 # Beginning of the next section. Re-establish the variables and run heston agian. 
-N_steps = 250
-N_sims = 500
-S_T,_,_ = Final_Heston_Sim(S0=S0, v0=v0, r=r, k=k, theta=theta, sigma=sigma, rho=rho, T=T, N_steps=N_steps, N_sims=N_sims)
+N_steps_const = 250
+N_sims_const = 500
+S_T,_,_ = Final_Heston_Sim(S0=S0, v0=v0, r=r, k=k, theta=theta, sigma=sigma, rho=rho, T=T, N_steps=N_steps_const, N_sims=N_sims_const)
 Call_price, Put_price = payoff(S_T,r=r, T=T, K=K)
 
+
 df = pd.DataFrame({
-    "Option Type": ["Call Option Price", "Put Option Price"],
-    "Price": [Call_price, Put_price]
+    "Option Type": ["Mean Asset Price at Maturity", "Call Option Price", "Put Option Price"],
+    "Price": [np.mean(S_T), Call_price, Put_price]
 })
 
-st.table(df)
-
-    # - \text{{Call Option Price}} = {Call_price}
-    # - \text{{Put Option Price}} = {Put_price}
+col1, col2 = st.columns([1,1], gap="small")
+with col1:
+    st.dataframe(df, use_container_width=True)
 
 st.info("This page is a work in progress! I am working on adding more depth to this analysis. Stay tuned!")
